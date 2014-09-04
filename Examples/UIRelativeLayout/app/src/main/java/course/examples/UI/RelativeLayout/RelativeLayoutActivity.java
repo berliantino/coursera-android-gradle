84c8d7ee172665fc7e3ddf672d1026d4b121ddc1
Michal Bialas <mmbialas@gmail.com>
2014-09-05 01:21:30 +0200

    Center button added to the main.xml

--- Examples/UIRelativeLayout/app/src/main/java/course/examples/UI/RelativeLayout/RelativeLayoutActivity.java
+++ Examples/UIRelativeLayout/app/src/main/java/course/examples/UI/RelativeLayout/RelativeLayoutActivity.java
@@ -1,6 +1,7 @@
 package course.examples.UI.RelativeLayout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,28 +10,37 @@ import android.widget.EditText;
 
 public class RelativeLayoutActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		final EditText textEntry = (EditText) findViewById(R.id.entry);
+        final EditText textEntry = (EditText) findViewById(R.id.entry);
 
-		final Button cancelButton = (Button) findViewById(R.id.cancel_button);
-		cancelButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				// Clear the textEntry
-				textEntry.setText("");
-			}
-		});
+        final Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Clear the textEntry
+                textEntry.setText("");
+            }
+        });
 
-		final Button okButton = (Button) findViewById(R.id.ok_button);
-		okButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				// Finish the application
-				RelativeLayoutActivity.this.finish();
-			}
-		});
-	}
+        final Button okButton = (Button) findViewById(R.id.ok_button);
+        okButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Finish the application
+                RelativeLayoutActivity.this.finish();
+            }
+        });
+
+        final Button centerButton = (Button) findViewById(R.id.center_button);
+        centerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RelativeLayoutActivity.this, ButtonsInCenterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
